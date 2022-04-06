218eb23db9d35bf1abdb3a77c5e6e98ef153a1b2
fnanni-0 <fnanni@itba.edu.ar>
2022-04-06 10:57:44 -0300

    refactor: tournament contract

--- contracts/Tournament.sol
+++ contracts/Tournament.sol
@@ -1,106 +1,127 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import "@reality.eth/contracts/development/contracts/IRealitio.sol";
+pragma solidity 0.8.13;
+import "@reality.eth/contracts/development/contracts/RealityETH-3.0.sol";
+
+// If a version for mainnet was needed, gas could be saved by storing merkle hashes instead of
+// all the questions and bets.
 
 contract Tournament {
     string public name;
-    address public _owner;
-    address public arbitrator;
-    IRealitio public realitio;
+    address public owner;
+    RealityETH_v3_0 public realitio; // Realitio v3
     bool public started;
-    uint256 public gamesCount = 0;
+    bool public initialized;
+    uint256 public resultSubmissionPeriodStart;
+    uint256 public minBet;
+    uint256 public closingTime;
+    uint256 public submissionTimeout;
 
-    struct Game {
-        string home; // home team name identifier
-        string away; // away team name identifier
-        uint32 minEnd; // min date to ask for a result
-        uint256 result; // 0 = Tie; 1=Home; 2=Away; 0xff..ff=invalid;
-        bytes32 questionID; // realitio question identifier
-    }
-
-    // Game[] private games; // array containing all the matches in the tournament
-    mapping(uint256 => Game) public games;
+    bytes32[] public questionIDs;
+    mapping(address => bytes32[][]) public bets;
+    mapping(address => uint256[]) public betsAmount;
 
     constructor(
         string memory _name,
-        address _arbitrator,
-        address _realityETH
+        address _realityETH,
+        uint256 _minBet,
+        uint256 _closingTime,
+        uint256 _submissionTimeout
     ) {
         name = _name;
-        _owner = msg.sender;
-        arbitrator = _arbitrator;
-        started = false;
-        realitio = IRealitio(_realityETH);
+        owner = msg.sender;
+        realitio = RealityETH_v3_0(_realityETH);
+        minBet = _minBet;
+        closingTime = _closingTime;
+        submissionTimeout = _submissionTimeout;
     }
 
-    function startTournament() public {
-        require(gamesCount >= 1, "At least one match it's needed to start");
-        started = true;
+    // Link all Realitio questions.
+    // SHould we add a weight for each question/answer?
+    function initializeTournament(bytes32[] calldata _questionIDs) external {
+        require(msg.sender == owner, "Not authorized");
+        require(!initialized, "Already initialized");
+
+        for (uint256 i = 0; i < _questionIDs.length; i++) {
+            require(realitio.getTimeout(_questionIDs[i]) > 0, "Question not created");
+        }
+
+        questionIDs = _questionIDs;
+
+        initialized = true;
     }
 
-    // Internal function to update the result of a match. Must be called from the
-    // function that reads the realitio answer.
-    function _updateGamehResult(uint256 _gameID, bytes32 _result) internal {
-        Game storage game = games[_gameID];
-        game.result = uint256(_result);
+    function placeBet(bytes32[] calldata _results) external payable {
+        require(initialized, "Not initialized");
+        require(_results.length == questionIDs.length, "Results mismatch");
+        require(msg.value >= minBet, "Not enough funds");
+        require(block.timestamp < closingTime, "Bets not allowed");
+
+        bets[msg.sender].push(_results);
+        betsAmount[msg.sender].push(msg.value);
     }
 
-    // Ask question to realitio. This must be called when initializing a match
-    function _askQuestion(
-        string memory home,
-        string memory away,
-        uint32 _minEnd
-    ) internal returns (bytes32) {
-        bytes32 delim = "\u241f";
-        string memory question = string(
-                abi.encodePacked(
-                    "Who has won (without considering penalties definition) the match between ",
-                    home,
-                    " (HOME) and ",
-                    away,
-                    " (AWAY) in the tournament ",
-                    name,
-                    "?",
-                    delim,
-                    "Tie",
-                    delim,
-                    "Home",
-                    delim,
-                    "Away",
-                    delim,
-                    "Sports",
-                    delim,
-                    "en"
-                )
-            );
-        bytes32 questionID = realitio.askQuestion(
-            2,
-            question,
-            arbitrator,
-            1 days,
-            _minEnd,
-            0
+    function registerAvailabilityOfResults() external {
+        require(block.timestamp > closingTime, "Bets not allowed");
+
+        for (uint256 i = 0; i < questionIDs.length; i++) {
+            bytes32 questionId = questionIDs[i];
+            realitio.resultForOnceSettled(questionId); // Reverts if not finalized.
+        }
+
+        resultSubmissionPeriodStart = block.timestamp;
+    }
+
+    function reopenQuestion(
+        uint256 questionIndex,
+        uint256 template_id, 
+        string memory question, 
+        address arbitrator, 
+        uint32 timeout, 
+        uint32 opening_ts, 
+        uint256 nonce,
+        uint256 min_bond,
+        address author
+    ) external {
+        //require parameters to calculate/check the previous questionId etc
+        bytes32 content_hash = keccak256(abi.encodePacked(template_id, opening_ts, question));
+        bytes32 question_id = keccak256(abi.encodePacked(content_hash, arbitrator, timeout, min_bond, address(realitio), author, nonce));
+
+        require(question_id == questionIDs[questionIndex], "Incorrect question data");
+        require(realitio.isSettledTooSoon(question_id), "Cannot reopen question");
+
+        bytes32 reopenedQuestionID = realitio.reopenQuestion(
+            template_id, 
+            question, 
+            arbitrator, 
+            timeout, 
+            opening_ts, 
+            nonce + 1, 
+            min_bond,
+            question_id
         );
-        return questionID;
     }
 
-    function createGame(
-        string memory _home,
-        string memory _away,
-        uint32 _minEnd
-    ) external returns (uint256) {
-        require(started == false, "Couldn't add match in a started tournament");
-        uint256 gameID = gamesCount + 1;
-        bytes32 questionID = _askQuestion(_home, _away, _minEnd);
-        Game memory game = Game({
-            home: _home,
-            away: _away,
-            minEnd: _minEnd,
-            result: 2**256 - 1, // initialize with invalid
-            questionID: questionID
-        });
-        games[gameID] = game;
-        gamesCount += 1;
-        return gameID;
+    function registerPoints(address _account, uint256 _betNumber) external {
+        require(resultSubmissionPeriodStart != 0, "Not in submission period");
+        require(block.timestamp < resultSubmissionPeriodStart + submissionTimeout, "Submission period over");
+        uint256 totalPoints;
+        for (uint256 i = 0; i < questionIDs.length; i++) {
+            bytes32 questionId = questionIDs[i];
+            bytes32 result = realitio.resultForOnceSettled(questionId); // Reverts if not finalized.
+            if (result == bets[_account][_betNumber][i]) {
+                totalPoints += 1;
+            }
+        }
+
+        // Check if winner
+
+        // Register winner
     }
+
+    function claimRewards(address _account, uint256 _betNumber) external {
+        require(resultSubmissionPeriodStart != 0, "Not in claim period");
+        require(block.timestamp > resultSubmissionPeriodStart + submissionTimeout, "Submission period not over");
+
+    }
+
 }
