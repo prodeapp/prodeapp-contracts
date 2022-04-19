9b72506564f240fd22c38843c2011661a797f70d
fnanni-0 <fnanni@itba.edu.ar>
2022-04-19 17:51:07 -0300

    refactor: downgrade to solidity 8.9

--- contracts/Tournament.sol
+++ contracts/Tournament.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@reality.eth/contracts/development/contracts/RealityETH-3.0.sol";
