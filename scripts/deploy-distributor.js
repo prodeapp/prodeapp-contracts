36614a854f60c3881ace6b0081e3ae811a92afa9
fnanni-0 <fnanni@itba.edu.ar>
2022-05-04 10:02:18 -0300

    refactor: add distributor contract

---
+++ scripts/deploy-distributor.js
@@ -0,0 +1,38 @@
+
+const hre = require("hardhat");
+const ethers = hre.ethers;
+
+const recipients = {
+  42: [],
+  100: []
+};
+
+async function main() {
+  const chainId = hre.network.config.chainId;
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log("Chain Id:", chainId);
+
+  // Deploy Tournament contract implementation
+  const Distributor = await ethers.getContractFactory("Distributor");
+  const distributor = await Distributor.deploy(recipients[chainId]);
+
+  console.log("Distributor address:", distributor.address);
+
+  // Verify contracts
+  await hre.run("verify:verify", {
+    address: distributor.address,
+    constructorArguments: [
+      recipients[chainId]
+    ],
+  });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
