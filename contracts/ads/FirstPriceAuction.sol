07d9c481ca85f8a7486e30aab468be71adf10f42
fnanni-0 <fnanni@itba.edu.ar>
2022-06-20 11:28:02 -0300

    refactor: add update bid function + fixes

--- contracts/ads/FirstPriceAuction.sol
+++ contracts/ads/FirstPriceAuction.sol
@@ -23,12 +23,9 @@ contract FirstPriceAuction {
 		uint256 balance;
 		bytes32 itemID; // on curate
 		address market;
-		//bytes32 adID;
-		//bytes32 tokenID;
 	}
 
 	bytes32 public constant QUEUE_START = 0x0000000000000000000000000000000000000000000000000000000000000000;
-	bytes32 public constant QUEUE_END = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 	uint256 public constant MIN_OFFER_DURATION = 60; // 1 min
 
 	ICurate public curate;
@@ -44,19 +41,19 @@ contract FirstPriceAuction {
 		bytes32 _itemID,
 		address _market,
 		uint256 _bidPerSecond
-		//bytes32 _startLookupBidID
 	) external payable {
 		require(msg.value / _bidPerSecond > MIN_OFFER_DURATION, "Not enough funds");
 		require(curate.isRegistered(_itemID), "Item must be registered");
 
 		bytes32 bidID = keccak256(abi.encode(_market, _itemID, msg.sender));
 		Bid storage newBid = bids[bidID];
-		require(newBid.bidder != address(0x0) || newBid.removed, "Bid is active");
+		require(newBid.bidder == address(0x0) || newBid.removed, "Bid is active");
 		newBid.bidder = msg.sender;
 		newBid.bidPerSecond = _bidPerSecond;
 		newBid.balance = msg.value;
 		newBid.itemID = _itemID;
 		newBid.market = _market;
+		newBid.removed = false;
 
 		_insertBid(_market, bidID);
 	}
@@ -65,6 +62,8 @@ contract FirstPriceAuction {
 		bytes32 bidID = keccak256(abi.encode(_market, _itemID, msg.sender));
 		Bid storage bid = bids[bidID];
 		bid.balance += msg.value;
+		require(curate.isRegistered(_itemID), "Item must be registered");
+		require(bid.bidder == msg.sender, "Bid does not exist");
 		require(bid.balance / bid.bidPerSecond > MIN_OFFER_DURATION, "Not enough funds");
 
 		if (bid.removed) {
@@ -73,13 +72,44 @@ contract FirstPriceAuction {
 		}
 	}
 
-	function updateBid(bytes32 _itemID, address _market, uint256 _bidPerSecond) external {
+	function updateBid(bytes32 _itemID, address _market, uint256 _bidPerSecond) external payable {
+		bytes32 bidID = keccak256(abi.encode(_market, _itemID, msg.sender));
+		Bid storage bid = bids[bidID];
+		require(curate.isRegistered(_itemID), "Item must be registered");
+		require(bid.bidder == msg.sender, "Bid does not exist");
 
+		bytes32 startID = keccak256(abi.encode(_market, QUEUE_START));
+		if (bid.previousBidPointer == startID && !bid.removed) {
+			// send balance back
+			uint256 price = (block.timestamp - bid.startTimestamp) * bid.bidPerSecond;
+			uint256 bill = price > bid.balance ? bid.balance : price;
+			bid.startTimestamp = 0;
+			bid.balance -= bill;
+			billing.send(bill);
+
+			if (bid.nextBidPointer != 0x0) {
+				Bid storage newHighestBid = bids[bid.nextBidPointer];
+				newHighestBid.startTimestamp = uint64(block.timestamp);
+			}
+		}
+
+		bid.balance += msg.value;
+		require(bid.balance / _bidPerSecond > MIN_OFFER_DURATION, "Not enough funds");
+		bid.bidPerSecond = _bidPerSecond;
+
+		// force remove and insert back
+		bids[bid.nextBidPointer].previousBidPointer = bid.previousBidPointer;
+		bids[bid.previousBidPointer].nextBidPointer = bid.nextBidPointer;
+		bid.previousBidPointer = 0x0;
+		bid.nextBidPointer = 0x0;
+		bid.removed = false;
+		_insertBid(_market, bidID);
 	}
 
 	function removeBid(bytes32 _itemID, address _market) external {
 		bytes32 bidID = keccak256(abi.encode(_market, _itemID, msg.sender));
 		Bid storage bid = bids[bidID];
+		require(bid.bidder == msg.sender, "Bid does not exist");
 		require(!bid.removed, "Bid already removed");
 		bid.removed = true;
 		bids[bid.nextBidPointer].previousBidPointer = bid.previousBidPointer;
@@ -104,7 +134,7 @@ contract FirstPriceAuction {
 		bid.balance = 0;
 	}
 
-	function reportDrainedBid(address _market) public {
+	function reportBid(address _market) public {
 		bytes32 startID = keccak256(abi.encode(_market, QUEUE_START));
 		bytes32 highestBidID = bids[startID].nextBidPointer;
 		require(highestBidID != 0x0, "No bid found");
@@ -116,7 +146,10 @@ contract FirstPriceAuction {
 		
 		// send balance back
 		uint256 price = (block.timestamp - highestBid.startTimestamp) * highestBid.bidPerSecond;
-		require(price >= highestBid.balance, "Highest bid is still active");
+		require(
+			price >= highestBid.balance || !curate.isRegistered(highestBid.itemID), 
+			"Highest bid is still active"
+		);
 		highestBid.startTimestamp = 0;
 		billing.send(highestBid.balance);
 		highestBid.balance = 0;
@@ -125,9 +158,7 @@ contract FirstPriceAuction {
 			Bid storage newHighestBid = bids[highestBid.nextBidPointer];
 			newHighestBid.startTimestamp = uint64(block.timestamp);
 		}
-
 	}
-
 
 	function _insertBid(address _market, bytes32 _bidID) internal {
 		// Insert the bid in the ordered list.
@@ -143,9 +174,7 @@ contract FirstPriceAuction {
 		bids[currentID].nextBidPointer = _bidID;
 		bid.previousBidPointer = currentID;
 		bid.nextBidPointer = nextID;
-		if (nextID != 0x0) {
-			bids[nextID].previousBidPointer = _bidID;
-		}
+		bids[nextID].previousBidPointer = _bidID;
 
 		if (currentID == startID) {
 			// Hightest bid! Activate the new bid and process the previous highest bid.
@@ -161,8 +190,8 @@ contract FirstPriceAuction {
 				if (bids[nextID].balance == 0) {
 					// remove bid from list.
 					bids[nextID].removed = true;
-					bids[bids[nextID].nextBidPointer].previousBidPointer = bids[nextID].previousBidPointer;
-					bids[bids[nextID].previousBidPointer].nextBidPointer = bids[nextID].nextBidPointer;
+					bids[bids[nextID].nextBidPointer].previousBidPointer = _bidID;
+					bids[_bidID].nextBidPointer = bids[nextID].nextBidPointer;
 
 				}
 			}
@@ -175,8 +204,7 @@ contract FirstPriceAuction {
 		if (highestBidID == 0x0) {
 			return "";
 		} else {
-			Bid storage bid = bids[highestBidID]; // highest bid might have expired.
-
+			Bid storage bid = bids[highestBidID];
 			ISVGContract svgContract = ISVGContract(curate.getAddress(bid.itemID));
 			return svgContract.getSVG();
 		}
