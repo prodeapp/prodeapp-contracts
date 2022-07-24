87869185aafaa89ef80cd02e5f526abaca998273
Santiago Algozino <salgozino@gmail.com>
2022-07-24 20:54:08 -0300

    fix graph query to updated version

--- scripts/register-ranking.js
+++ scripts/register-ranking.js
@@ -17,7 +17,7 @@ const BATCHER_ABI = [
   }
 ]
 
-const tournamentAddress = "0xb53c73b48294f5c3c9025f65b247ce8d50c4d345";
+const marketAddress = "0x48cca4b7e5e1c38353295966f6c6b1b1d462b775";
 const transactionBatcher = "0xA73A872eFD768bb23efb24CEeB9e330bcCA259D6";
 const subgraphUrl = "https://api.thegraph.com/subgraphs/name/prodeapp/prodeapp";
 
@@ -30,26 +30,24 @@ async function main() {
 
   const graph = new GraphQLClient(subgraphUrl);
 
-  const { bets, tournament: tournamentData } = await graph.request(
+  const { bets, market: marketData } = await graph.request(
     gql`
-      query rankingQuery($tournamentAddressStart: String, $tournamentAddressEnd: String) {
-        bets(where: {id_gt: $tournamentAddressStart, id_lt: $tournamentAddressEnd, points_not: 0}, orderBy: points, orderDirection: desc) {
+      query rankingQuery($marketAddress: String) {
+        bets(where: {market: $marketAddress, points_not: 0}, orderBy: points, orderDirection: desc) {
           tokenID
           points
         }
-        tournament(id: $tournamentAddressStart) {
+        market(id: $marketAddress) {
           prizes
         }
       }
     `,
       {
-        tournamentAddressStart: tournamentAddress,
-        tournamentAddressEnd: tournamentAddress.replace(/.$/,"Z"),
+        marketAddress: marketAddress
       }
   );
-
   const Tournament = await ethers.getContractFactory("Tournament");
-  const tournament = await Tournament.attach(tournamentAddress);
+  const tournament = await Tournament.attach(marketAddress);
 
   const passPeriod = (await tournament.populateTransaction.registerAvailabilityOfResults()).data;
   let datas = [passPeriod];
@@ -57,17 +55,17 @@ async function main() {
   let previousPoints = bets[0].points;
   for (let i = 0; i < bets.length; i++) {
     rankIndex = previousPoints == bets[i].points ? rankIndex : i;
-    if (rankIndex >= tournamentData.prizes.length) break;
+    if (rankIndex >= marketData.prizes.length) break;
 
     const registerPoints = (await tournament.populateTransaction.registerPoints(bets[i].tokenID, rankIndex, i - rankIndex)).data;
     datas.push(registerPoints);
-    
+   
     previousPoints = bets[i].points;
   }
 
   const batcherContract = new ethers.Contract(transactionBatcher, BATCHER_ABI, signer);
   await batcherContract.batchSend(
-    Array(datas.length).fill(tournamentAddress),
+    Array(datas.length).fill(marketAddress),
     Array(datas.length).fill(0),
     datas
   );
