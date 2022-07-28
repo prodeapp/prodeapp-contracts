955c759fa5eb5a3890f5229bbcc73124f2a231b2
Santiago Algozino <salgozino@gmail.com>
2022-07-27 21:50:08 -0300

    scripts: add F1 teams and drivers

---
+++ scripts/drivers.js
@@ -0,0 +1,29 @@
+const DRIVERS = {
+  F1: {
+    VERSTAPPEN: 'Max Verstappen',
+    PEREZ: 'Sergio Perez',
+    LECLERC: 'Charles Leclerc',
+    SAINZ: 'Carlos Sainz',
+    RUSSEL: 'George Russel',
+    HAMILTON: 'Lewis Hamilton',
+    OCON: 'Esteban Ocon',
+    ALONSO: 'Fernando Alonso',
+    NORRIS: 'Lando Norris',
+    RICCIARDO: 'Daniel Ricciardo',
+    ZHOU: 'Zhou Guanyu',
+    BOTTAS: 'Valtteri Bottas',
+    MAGNUSSEN: 'Kevin Magnussen',
+    SCHUMACHER: 'Mick Schumacher',
+    GASLY: 'Pier Gasly',
+    TSUNODA: 'Yuki Tsunoda',
+    VETTEL: 'Sebastian Vettel',
+    STROLL: 'Lance Stroll',
+    ALBON: 'Alexander Albon',
+    LATIFI: 'Nicholas Latifi',
+    HULKENBERG: 'Nico Hulkenberg'
+  }
+};
+
+module.exports = {
+  DRIVERS,
+}
