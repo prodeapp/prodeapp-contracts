9ce517d44bfb0cc70ce8fb35a33c239366e32cf2
leuillades <leuillades@gmail.com>
2022-07-28 10:33:46 -0300

    Added engines and tires modules

---
+++ scripts/f1_engines.js
@@ -0,0 +1,13 @@
+const ENGINES = {
+    F1: {
+      FERRARI: 'Ferrari',
+      MERCEDES: 'Mercedes Benz',
+      REDBULL: 'Red Bull',
+      RENAULT: 'Renault',
+    }
+  };
+  
+  module.exports = {
+    ENGINES,
+  }
+  
