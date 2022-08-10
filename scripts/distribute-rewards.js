8b8b04f18e4cdcdaed4f040de2ddd685d8983ba8
Rodrigo Souto <rodrigo_7@live.com.ar>
2022-08-09 22:29:42 -0300

    distribute rewards for all pending markets

--- scripts/distribute-rewards.js
+++ scripts/distribute-rewards.js
@@ -17,19 +17,52 @@ const BATCHER_ABI = [
   }
 ]
 
-const marketAddress = "";
 const transactionBatcher = "0xA73A872eFD768bb23efb24CEeB9e330bcCA259D6";
 const subgraphUrl = "https://api.thegraph.com/subgraphs/name/prodeapp/prodeapp";
+const graph = new GraphQLClient(subgraphUrl);
 
 async function main() {
   const chainId = hre.network.config.chainId;
   const [signer] = await ethers.getSigners();
 
-  console.log("Account balance:", (await signer.getBalance()).toString());
+  console.log("Account balance:", ethers.utils.formatUnits((await signer.getBalance()).toString(), 18));
   console.log("Chain Id:", chainId);
 
-  const graph = new GraphQLClient(subgraphUrl);
+  // fetch candidate markets
+  let {markets} = await graph.request(
+    gql`
+      query marketsQuery {
+        markets(where: {resultSubmissionPeriodStart_gt: 0}) {
+          id
+          name
+          resultSubmissionPeriodStart
+          submissionTimeout
+          bets {
+            claim
+          }
+        }
+      }
+    `
+  );
 
+  const now = Math.floor(Date.now() / 1000);
+
+  markets = markets.filter(market => now > (Number(market.resultSubmissionPeriodStart) + Number(market.submissionTimeout)))
+
+  if (markets.length === 0) {
+    console.log('No markets found');
+    return;
+  }
+
+  for (const market of markets) {
+    console.log(`${market.name} (${market.id})`);
+    await processMarket(market.id, market.bets.filter(bet => bet.claim).length, signer);
+  }
+}
+
+async function processMarket(marketAddress, totalClaimed, signer) {
+  // TODO: we are reading all the markets, we will need to optimize this if we reach a big amount of markets
+  // (maybe keep track of the markets distributed or limit to the markets closed the last few days)
   const { bets, market: marketData } = await graph.request(
     gql`
       query rankingQuery($marketAddress: String) {
@@ -58,22 +91,29 @@ async function main() {
       if (currentRankPoints > bets[i].points) {break;}
       else {endSharedIndex = i;}
     }
+
     for (let j=0; j<=endSharedIndex-firstSharedIndex; j++){
       // console.log('Claim inputs:', rankIndex+j, currentRankPoints, firstSharedIndex, endSharedIndex)
       const claimReward = (await market.populateTransaction.claimRewards(rankIndex+j, firstSharedIndex, endSharedIndex)).data;
       datas.push(claimReward);
     }
-    
+
     if (endSharedIndex >= marketData.prizes.length) break;
     firstSharedIndex = endSharedIndex+1
     endSharedIndex = firstSharedIndex
   }
-  
+
   if (endSharedIndex < marketData.prizes.length) {
     // console.log("Distributing remaining prizes for this market")
     const remainingPrizes = (await market.populateTransaction.distributeRemainingPrizes()).data;
     datas.push(remainingPrizes);
   } else if (datas.length == 0) {
+    console.log('nothing to distribute');
+    return;
+  }
+
+  if (totalClaimed === datas.length) {
+    console.log('prizes already claimed');
     return;
   }
 
@@ -83,6 +123,7 @@ async function main() {
     Array(datas.length).fill(0),
     datas
   );
+  console.log('distribution ok!');
 }
 
 main()
