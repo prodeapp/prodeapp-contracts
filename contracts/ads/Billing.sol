ba65007bf93541384cf4aef49b6bed2f849a2443
fnanni-0 <fnanni@itba.edu.ar>
2022-08-29 10:13:11 -0300

    refactor: billing contract v1

--- contracts/ads/Billing.sol
+++ contracts/ads/Billing.sol
@@ -4,36 +4,51 @@ pragma solidity 0.8.9;
 import "./../IMarket.sol";
 
 contract Billing {
-    struct Market {
-        uint256 balance;
-    }
-
 	address public immutable firstPriceAuctionContract;
 
-    mapping(IMarket => Market) public markets;
+    address public governor;
+    address public beneficiary;
+    mapping(IMarket => uint256) public balances;
 
-    constructor(address _firstPriceAuctionContract) {
+    constructor(
+		address _firstPriceAuctionContract,
+		address _governor,
+		address _beneficiary
+	) {
 		firstPriceAuctionContract = _firstPriceAuctionContract;
+		governor = _governor;
+		beneficiary = _beneficiary;
 	}
+
+    function changeGovernor(address _governor) external {
+        require(msg.sender == governor, "Not authorized");
+        governor = _governor;
+    }
+
+    function changeBeneficiary(address _beneficiary) external {
+        require(msg.sender == governor, "Not authorized");
+        beneficiary = _beneficiary;
+    }
 
     /** @dev Creates and places a new bid or replaces one that has been removed.
      *  @param _market The address of the market the bid will be placed to.
      */
     function registerPayment(IMarket _market) external payable {
-        require(msg.sender == firstPriceAuctionContract, "Not authorized");
-		markets[_market].balance += msg.value;
+		balances[_market] += msg.value;
     }
 
     /** @dev Creates and places a new bid or replaces one that has been removed.
      *  @param _market The address of the market the bid will be placed to.
      */
     function executePayment(IMarket _market) external payable {
-		if (_market.resultSubmissionPeriodStart() == 0) {
-			uint256 amount = markets[_market].balance;
-			markets[_market].balance = 0;
-			_market.fundMarket{value: amount}("");
-		} else {
+		uint256 revenue = balances[_market];
+		balances[_market] = 0;
 
+		try _market.fundMarket{value: revenue}("ads") {
+			// _market.resultSubmissionPeriodStart() == 0
+		} catch {
+			(bool success, ) = beneficiary.call{value: revenue}(new bytes(0));
+			require(success, "Send XDAI failed");
 		}
     }
 }
