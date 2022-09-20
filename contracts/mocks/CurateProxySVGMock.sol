835db913609b3f7f1e9be1e3ae6abc4e5eca18fb
Algozino Santiago <salgozino@gmail.com>
2022-09-19 22:40:51 -0300

    add event and registerAd method

--- contracts/mocks/CurateProxySVGMock.sol
+++ contracts/mocks/CurateProxySVGMock.sol
@@ -5,6 +5,13 @@ contract CurateProxySVGMock {
     mapping(bytes32 => address) private items;
     mapping(bytes32 => bool) private status;
 
+    event newItemMapped(
+        address indexed _svgAddress,
+        bytes32 indexed _itemID,
+        bytes32 _contentItemID,
+        bytes32 _technicalItemID
+    );
+
     constructor() public {}
 
     function setItem(
@@ -12,8 +19,22 @@ contract CurateProxySVGMock {
         address _address,
         bool _status
     ) external {
+        if (items[_itemID] == address(0x0)) {
+            // new item
+            emit newItemMapped(
+                _address,
+                _itemID,
+                keccak256(abi.encodePacked(address(0x0))),
+                keccak256(abi.encodePacked(address(0x0)))
+            );
+        }
         items[_itemID] = _address;
         status[_itemID] = _status;
+    }
+
+    function registerAd(bytes32 _contentItemID, bytes32 _technicalItemID) external {
+        bytes32 itemID = keccak256(abi.encode(_contentItemID, _technicalItemID));
+        this.setItem(itemID, msg.sender, true);
     }
 
     function isRegistered(bytes32 _itemID) public view returns (bool) {
