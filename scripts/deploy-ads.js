63b5841c29cbc6266b54211a6272712269be153e
fnanni-0 <fnanni@itba.edu.ar>
2022-09-27 00:44:05 -0300

    chore: ads

--- scripts/deploy-ads.js
+++ scripts/deploy-ads.js
@@ -3,29 +3,17 @@ const ethers = hre.ethers;
 
 const params = {
     42: {
-        arbitrator: "0xDEd12537dA82C1019b3CA1714A5d58B7c5c19A04",
-        realityEth: "0xcB71745d032E16ec838430731282ff6c10D29Dea",
-        curate: "",
         governor: "",
-        submissionTimeout: 1 * 24 * 60 * 60,
         curateContent: "0x0000000000000000000000000000000000000000",  // TODO: Update to curate list address
         curateTechnical: "0x0000000000000000000000000000000000000000"
     },
     100: {
-        arbitrator: "0x29F39dE98D750eb77b5FAfb31B2837f079FcE222",
-        realityEth: "0xE78996A233895bE74a66F451f1019cA9734205cc",
-        curate: "0x86E72802D9AbBF7505a889721fD4D6947B02320E",
-        governor: "0x0029ec18568F96AFE25Ea289Dac6c4703868924d",
-        submissionTimeout: 7 * 24 * 60 * 60,
+        governor: "",
         curateContent: "0x76D159faDCDE966d68885b7267Ce42F4F655238D",  // TODO: Update to curate list address
-        curateTechnical: "0x950B8Be040B686bCB6cF9ee24D4B2E65ae6fe5fC"
+        curateTechnical: "0x6E6Bc2032d5A19728Aeb30f06507e7Cb43e4bBFD"
     },
     31337: {
-        arbitrator: "0x29F39dE98D750eb77b5FAfb31B2837f079FcE222",
-        realityEth: "0xE78996A233895bE74a66F451f1019cA9734205cc",
-        curate: "0x86E72802D9AbBF7505a889721fD4D6947B02320E",
         governor: "0x0029ec18568F96AFE25Ea289Dac6c4703868924d",
-        submissionTimeout: 7 * 24 * 60 * 60,
         curateContent: "0x0000000000000000000000000000000000000000",  // TODO: Update to curate list address
         curateTechnical: "0x0000000000000000000000000000000000000000"
     }
@@ -41,28 +29,30 @@ async function main() {
     console.log("Chain Id:", chainId);
 
     // Curate SVG Proxy
-    // TODO: Change to not Mock proxy!
-    const CurateProxy = await ethers.getContractFactory("CurateProxySVGMock");
-    const curateProxy = await CurateProxy.deploy();
+    const CurateProxy = await ethers.getContractFactory("CurateProxySVG");
+    const curateProxy = await CurateProxy.deploy(
+        params[chainId].curateTechnical,
+        params[chainId].curateContent
+    );
     await curateProxy.deployed();
     console.log("Curate Proxy SVG deployed at ", curateProxy.address);
 
     // Deploy SVG contract
-    const Base64Ad = await ethers.getContractFactory("Base64Ad");
-    const base64Ad = await Base64Ad.deploy();
-    await base64Ad.deployed();
-    console.log("Base64Ad deployed at ", base64Ad.address);
+    const SVG = await ethers.getContractFactory("SVG");
+    const svg = await SVG.deploy();
+    await svg.deployed();
+    console.log("SVG deployed at ", svg.address);
 
     // Deploy SVG Factory contract
-    const Base64AdFactory = await ethers.getContractFactory("Base64AdFactory");
-    const base64AdFactory = await Base64AdFactory.deploy(
-        base64Ad.address,
+    const SVGFactory = await ethers.getContractFactory("SVGFactory");
+    const svgFactory = await SVGFactory.deploy(
+        svg.address,
         params[chainId].curateTechnical,
         params[chainId].curateContent,
         curateProxy.address
     );
-    await base64AdFactory.deployed();
-    console.log("Base64AdFactory deployed at ", base64AdFactory.address);
+    await svgFactory.deployed();
+    console.log("SVGFactory deployed at ", svgFactory.address);
 
     // Deploy Billing contract
     const Billing = await ethers.getContractFactory("Billing");
@@ -76,43 +66,16 @@ async function main() {
     await auctionContract.deployed();
     console.log("Auction deployed at ", auctionContract.address);
 
-
-    // Verify contracts
-    await hre.run("verify:verify", {
-        address: curateProxy.address,
-    });
-
-    await hre.run("verify:verify", {
-        address: base64Ad.address,
-    });
-
-    await hre.run("verify:verify", {
-        address: base64AdFactory.address,
-        constructorArguments: [
-            base64Ad.address,
-            params[chainId].curateTechnical,
-            params[chainId].curateContent,
-            curateProxy.address
-        ],
-    });
-
-    await hre.run("verify:verify", {
-        address: billing.address,
-        constructorArguments: [
-            params[chainId].governor,
-            params[chainId].governor
-        ],
-    });
-
-    await hre.run("verify:verify", {
-        address: auctionContract.address,
-        constructorArguments: [
-            curateProxy.address,
-            billing.address
-        ],
-    });
-
-
+    const verifyCurate = `npx hardhat verify --network gnosis ${curateProxy.address} "${params[chainId].curateTechnical}" "${params[chainId].curateContent}"`;
+    const verifyAd = `npx hardhat verify --network gnosis ${svg.address}`;
+    const verifyFactory = `npx hardhat verify --network gnosis ${svgFactory.address} "${svg.address}" "${params[chainId].curateTechnical}" "${params[chainId].curateContent}" "${curateProxy.address}"`;
+    const verifyBilling = `npx hardhat verify --network gnosis ${billing.address} "${params[chainId].governor}" "${params[chainId].governor}"`;
+    const verifyAuction = `npx hardhat verify --network gnosis ${auctionContract.address} "${curateProxy.address}" "${billing.address}"`;
+    console.log("");
+    console.log("Run the following to verify the contracts");
+    console.log();
+    console.log(`${verifyCurate} && ${verifyAd} && ${verifyFactory} && ${verifyBilling} && ${verifyAuction}`);
+    console.log();
 }
 
 main()
