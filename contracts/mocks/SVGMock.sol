4287e2333ec3029fcdacde60df820554bbdb6d28
Algozino Santiago <salgozino@gmail.com>
2022-09-27 19:15:44 -0300

    refactor: rename mocks for consistency with contracts

--- contracts/mocks/SVGMock.sol
+++ contracts/mocks/SVGMock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-contract Base64AdMock {
+contract SVGMock {
     string private svg;
 
     constructor() {}
--- test/ad-auction.js
+++ test/ad-auction.js
@@ -70,13 +70,13 @@ describe("Market", () => {
     await curateProxy.deployed();
 
     // Deploy SVG contract
-    const Base64Ad = await ethers.getContractFactory("Base64AdMock");
-    const base64Ad = await Base64Ad.deploy();
-    await base64Ad.deployed();
+    const SVG = await ethers.getContractFactory("SVGMock");
+    const svg = await SVG.deploy();
+    await svg.deployed();
 
     // Deploy SVG Factory contract
-    const Base64AdFactory = await ethers.getContractFactory("Base64AdFactoryMock");
-    const svgFactory = await Base64AdFactory.deploy(base64Ad.address);
+    const SVGFactory = await ethers.getContractFactory("SVGFactoryMock");
+    const svgFactory = await SVGFactory.deploy(svg.address);
     await svgFactory.deployed();
 
     // Deploy Billing contract
