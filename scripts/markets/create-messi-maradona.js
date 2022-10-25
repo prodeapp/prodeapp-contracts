0261522b887a36e8e6e89643e9186224fad8f87e
Algozino Santiago <salgozino@gmail.com>
2022-10-25 09:55:10 -0300

    add address and unify opening timestamp

--- scripts/markets/create-messi-maradona.js
+++ scripts/markets/create-messi-maradona.js
@@ -3,84 +3,85 @@ const ethers = hre.ethers;
 const {getChain, orderQuestions, toTimestamp} = require("./helpers");
 
 const timeout = 129600; // 1.5 days
+const openingTS = toTimestamp("2022-12-18 18:00:00 UTC");
 const marketData = {
   marketName: "Messi vs Maradona in World Cups", 
   marketSymbol: "PRODE",
   closingTime: toTimestamp("2022-11-19 00:00:00 UTC"),
   price: ethers.utils.parseUnits("1.0", "ether"), // 1 xDAI
-  creator: "", // Fork DAO address
+  creator: "0xa4400B1Ae4422cD2302d699F929C1710ACEf33d0", // Fork DAO address relayer
   creatorFee: 400, // 4%
   minBond: ethers.utils.parseUnits("5.0", "ether"), // 5 xDAI
   questions: [
     {
       templateID: 2, 
       question: "Will Messi score more goals in FWC Qatar 2022 than Maradona (5 goals) in FWC Mexico 1986?␟\"Yes\",\"No\",\"Tie\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi make more assists in FWC Qatar 2022 than Maradona (5 assists) in FWC Mexico 1986?␟\"Yes\",\"No\",\"Tie\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi have more goal involvements in FWC Qatar 2022 than Maradona (10 assists & goals) in FWC Mexico 1986?␟\"Yes\",\"No\",\"Tie\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi win the Adidas Golden Ball Award in FWC Qatar 2022?␟\"Yes\",\"No\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Argentina win the FIFA World Cup Qatar 2022?␟\"Yes\",\"No\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi be fouled more times in FWC Qatar 2022 than Maradona in FWC Mexico 1986?␟\"Yes\",\"No\",\"Tie\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
-    },,
+      openingTS: openingTS
+    },
     {
       templateID: 2, 
       question: "Will Messi achieve more successful take-ons in FWC Qatar 2022 than Maradona in FWC Mexico 1986?␟\"Yes\",\"No\",\"Tie\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi accumulate more goals in World Cups than Maradona after FWC Qatar 2022?␟\"Yes\",\"No\",\"Tie\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi accumulate more assists in World Cups than Maradona after FWC Qatar 2022?␟\"Yes\",\"No\",\"Tie\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi win an award as goalscorer at the FIFA World Cup Qatar 2022?␟\"Golden Boot\",\"Silver Boot\",\"Bronze Boot\",\"No Award\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi provoke more cards in FWC Qatar 2022 than Maradona (12) in FWC Mexico 1986?␟\"Yes\",\"No\",\"Tie\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi score at least 1 free kick goal at the FIFA World Cup Qatar 2022?␟\"Yes\",\"No\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi score at least 1 goal in the knockout phase matches at the FIFA World Cup Qatar 2022?␟\"Yes\",\"No\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     },
     {
       templateID: 2, 
       question: "Will Messi score more goals in knockout phase matches in FWC Qatar 2022 than Maradona (4 goals) in FWC Mexico 1986?␟\"Yes\",\"No\",\"Tie\"␟football␟en_US", 
-      openingTS: toTimestamp("2022-12-18 18:00:00 UTC")
+      openingTS: openingTS
     }
   ],
   prizeWeights: [7500, 2500]
