f0ac7451f8cdaf8dd1152de042dc53c59a0c4b2f
Algozino Santiago <salgozino@gmail.com>
2022-11-26 10:21:43 -0300

    add goal questions

--- scripts/markets/create-single-match-worldcup.js
+++ scripts/markets/create-single-match-worldcup.js
@@ -4,36 +4,39 @@ const ethers = hre.ethers;
 
 const timeout = 129600; // 1.5 days
 
-const marketName = "Argentina vs Mexico - FIFA World Cup Qatar 2022 - Group Stage";
-
-const player = 'Messi';
-const teams = ['Argentina', 'Mexico']
+const player = 'Cristiano Ronaldo';
+const teams = ['Portugal', 'Uruguay']
+const openingTs = toTimestamp("2022-11-28 16:00:00 GMT-3") + SOCCER_MATCH_DURATION;
+const marketName = `${teams[0]} vs ${teams[1]} - FIFA World Cup Qatar 2022 - Group Stage`;
 const teamsDraw = teams.concat('Draw');
 const teamsNone = teams.concat('None');
 const yesNo = ['Yes', 'No'];
 const yesNoDraw = yesNo.concat('Draw')
-const openingTs = toTimestamp("2022-11-26 16:00:00 GMT-3") + SOCCER_MATCH_DURATION;
+const goals = ["0", "1", "2", "3", "4", "5", "6", "7", ">7"];
+
 
 const marketData = {
   marketName: marketName,
   marketSymbol: "PRODE",
   closingTime: openingTs - SOCCER_MATCH_DURATION,
-  price: ethers.utils.parseUnits("2.0", "ether"), // 2 xDAI
+  price: ethers.utils.parseUnits("3.0", "ether"), // 3 xDAI
   creator: "0x0029ec18568F96AFE25Ea289Dac6c4703868924d",
   creatorFee: 300,
   minBond: ethers.utils.parseUnits("5.0", "ether"),  // 5 xDAI
   questions: [
     buildQuestionSingleSelect(`What will be the result of ${marketName}?`, teamsDraw, openingTs, 'soccer'),
-    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} win the match after trailing by at least one gol at ${marketName}?`, yesNo, openingTs + 1, 'soccer'),
-    buildQuestionSingleSelect(`Will ${player} score a goal at ${marketName}?`, yesNo, openingTs + 2, 'soccer'),
-    buildQuestionSingleSelect(`Will a goal at ${marketName} be ruled out due to a VAR decision?`, yesNo, openingTs + 3, 'soccer'),
-    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} score a goal in the extra time at ${marketName}?`, yesNo, openingTs + 4, 'soccer'),
-    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} score a goal from a free kick at ${marketName}?`, yesNo, openingTs + 5, 'soccer'),
-    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} score a header goal at ${marketName}?`, yesNo, openingTs + 6, 'soccer'),
-    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} score a penalty goal at ${marketName}?`, yesNo, openingTs + 7, 'soccer'),
-    buildQuestionSingleSelect(`Will ${teams[0]} receive more yellow cards than ${teams[1]} at ${marketName}?`, yesNoDraw, openingTs + 8, 'soccer'),
-    buildQuestionSingleSelect(`Will ${teams[0]} receive at least one red card at ${marketName}?`, yesNo, openingTs + 9, 'soccer'),
-    buildQuestionSingleSelect(`Will ${teams[1]} receive at least one red card at ${marketName}?`, yesNo, openingTs + 10, 'soccer'),
+    buildQuestionSingleSelect(`How many goals will score ${teams[0]} at ${marketName}?`, goals, openingTs+1, 'soccer'),
+    buildQuestionSingleSelect(`How many goals will score ${teams[1]} at ${marketName}?`, goals, openingTs+2, 'soccer'),
+    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} win the match after trailing by at least one gol at ${marketName}?`, yesNo, openingTs + 3, 'soccer'),
+    buildQuestionSingleSelect(`Will ${player} score a goal at ${marketName}?`, yesNo, openingTs + 4, 'soccer'),
+    buildQuestionSingleSelect(`Will a goal at ${marketName} be ruled out due to a VAR decision?`, yesNo, openingTs + 5, 'soccer'),
+    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} score a goal in the extra time at ${marketName}?`, yesNo, openingTs + 6, 'soccer'),
+    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} score a goal from a free kick at ${marketName}?`, yesNo, openingTs + 7, 'soccer'),
+    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} score a header goal at ${marketName}?`, yesNo, openingTs + 8, 'soccer'),
+    buildQuestionSingleSelect(`Will ${teams[0]} or ${teams[1]} score a penalty goal at ${marketName}?`, yesNo, openingTs + 9, 'soccer'),
+    buildQuestionSingleSelect(`Will ${teams[0]} receive more yellow cards than ${teams[1]} at ${marketName}?`, yesNoDraw, openingTs + 10, 'soccer'),
+    buildQuestionSingleSelect(`Will ${teams[0]} receive at least one red card at ${marketName}?`, yesNo, openingTs + 11, 'soccer'),
+    buildQuestionSingleSelect(`Will ${teams[1]} receive at least one red card at ${marketName}?`, yesNo, openingTs + 12, 'soccer'),
     
     
   ],
