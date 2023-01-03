05a1d1f83020421f9add4cc69a5f3676f3f5ea1b
fnanni-0 <fnanni@itba.edu.ar>
2023-01-03 00:22:55 -0300

    refactor: review

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -4,9 +4,10 @@ pragma solidity 0.8.9;
 import "./IMarket.sol";
 
 contract LiquidityPool {
-
     uint256 public constant DIVISOR = 10000;
+    uint256 private constant UINT_MAX = type(uint256).max;
 
+    bool public initialized;
     IMarket public market;
     address public creator;
     uint256 public creatorFee;
@@ -14,30 +15,41 @@ contract LiquidityPool {
     uint256 public betMultiplier; // how much the LP adds to the market pool for each $ added to the market
     uint256 public totalDeposits;
     uint256 public poolReward;
+    uint256 public creatorReward;
 
     mapping(address => uint256) private balances;
     mapping(uint256 => bool) private claims;
 
-    event Staked(address indexed user, uint256 amount);
-    event Withdrawn(address indexed user, uint256 amount);
+    event Staked(address indexed _user, uint256 _amount);
+    event Withdrawn(address indexed _user, uint256 _amount);
+    event LiquidityReward(address indexed _user, uint256 _reward);
     event BetReward(uint256 indexed _tokenID, uint256 _reward);
-    event MarketPaymentSent(address indexed market, uint256 amount);
-    event MarketPaymentReceived(address indexed market, uint256 _amount);
+    event MarketPaymentSent(address indexed _market, uint256 _amount);
+    event MarketPaymentReceived(address indexed _market, uint256 _amount);
 
-    constructor(address _creator, uint256 _creatorFee, uint256 _betMultiplier) {
+    constructor() {}
+
+    function initialize(
+        address _creator,
+        uint256 _creatorFee,
+        uint256 _betMultiplier,
+        address _market,
+        uint256 _pointsToWin
+    ) external {
+        require(!initialized, "Already initialized.");
         require(_creatorFee < DIVISOR, "Creator fee too big");
+        require(
+            _pointsToWin > 0 && _pointsToWin <= IMarket(_market).numberOfQuestions(),
+            "Invalid pointsToWin value"
+        );
 
         creator = _creator;
         creatorFee = _creatorFee;
         betMultiplier = _betMultiplier;
-    }
-
-    // TODO: refactor
-    function setMarket(address _market, uint256 _pointsToWin) external {
-        require(_pointsToWin > 0 && _pointsToWin <= IMarket(_market).numberOfQuestions(), "Invalid pointsToWin value");
-
         market = IMarket(_market);
         pointsToWin = _pointsToWin;
+
+        initialized = true;
     }
 
     function deposit() external payable {
@@ -50,26 +62,41 @@ contract LiquidityPool {
     }
 
     function withdraw() external {
+        require(market.nextTokenID() == 0, "Withdraw not allowed");
+
+        uint256 amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+        totalDeposits -= amount;
+
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function claimLiquidityRewards(address _account) external {
         require(market.resultSubmissionPeriodStart() != 0, "Withdraw not allowed");
-        require(balances[msg.sender] > 0, "Not enough balance");
+        require(
+            block.timestamp > market.resultSubmissionPeriodStart() + market.submissionTimeout(),
+            "Submission period not over"
+        );
+        require(balances[_account] > 0, "Not enough balance");
 
         uint256 amount;
 
         if (market.ranking(0).points >= pointsToWin) {
             // there's at least one winner, withdraw fees + excess deposit
-            uint256 maxPayment = market.price() * betMultiplier * market.nextTokenID();
+            uint256 maxPayment = mulCap(market.price(), market.nextTokenID());
+            maxPayment = mulCap(maxPayment, betMultiplier);
             uint256 excessDeposit = totalDeposits < maxPayment ? 0 : (totalDeposits - maxPayment);
 
-            amount = (poolReward + excessDeposit) * balances[msg.sender] / totalDeposits;
+            amount = ((poolReward + excessDeposit) * balances[_account]) / totalDeposits;
         } else {
             // withdraw fees + deposits
-            amount = address(this).balance * balances[msg.sender] / totalDeposits;
+            amount = ((poolReward + totalDeposits) * balances[_account]) / totalDeposits;
         }
 
-        balances[msg.sender] = 0;
+        balances[_account] = 0;
 
-        requireSendXDAI(payable(msg.sender), amount);
-        emit Withdrawn(msg.sender, amount);
+        requireSendXDAI(payable(_account), amount);
+        emit LiquidityReward(_account, amount);
     }
 
     /** @dev Sends a prize to the token holder if applicable.
@@ -77,7 +104,7 @@ contract LiquidityPool {
      *  @param _firstSharedIndex If there are many tokens sharing the same score, this is the first ranking position of the batch.
      *  @param _lastSharedIndex If there are many tokens sharing the same score, this is the last ranking position of the batch.
      */
-    function claimRewards(
+    function claimBettorRewards(
         uint256 _rankIndex,
         uint256 _firstSharedIndex,
         uint256 _lastSharedIndex
@@ -103,11 +130,16 @@ contract LiquidityPool {
         uint256 sharedBetween = _lastSharedIndex - _firstSharedIndex + 1;
 
         uint256 cumWeigths = 0;
-        for (uint256 i = _firstSharedIndex; i < market.getPrizes().length && i <= _lastSharedIndex; i++) {
+        for (
+            uint256 i = _firstSharedIndex;
+            i < market.getPrizes().length && i <= _lastSharedIndex;
+            i++
+        ) {
             cumWeigths += market.prizeWeights(i);
         }
 
-        uint256 maxPayment = market.price() * betMultiplier * market.nextTokenID();
+        uint256 maxPayment = mulCap(market.price(), market.nextTokenID());
+        maxPayment = mulCap(maxPayment, betMultiplier);
         uint256 marketPayment = totalDeposits < maxPayment ? totalDeposits : maxPayment;
 
         uint256 reward = (marketPayment * cumWeigths) / (DIVISOR * sharedBetween);
@@ -116,16 +148,43 @@ contract LiquidityPool {
         emit BetReward(market.ranking(_rankIndex).tokenID, reward);
     }
 
+    function executeCreatorRewards() external {
+        uint256 creatorRewardToSend;
+        if (totalDeposits == 0) {
+            // No liquidity was provided. Creator gets all the rewards
+            creatorRewardToSend = creatorReward + poolReward;
+            poolReward = 0;
+        } else {
+            creatorRewardToSend = creatorReward;
+        }
+        creatorReward = 0;
+        requireSendXDAI(payable(creator), creatorRewardToSend);
+    }
+
     function requireSendXDAI(address payable _to, uint256 _value) internal {
         (bool success, ) = _to.call{value: _value}(new bytes(0));
         require(success, "LiquidityPool: Send XDAI failed");
     }
 
+    /**
+     * @dev Multiplies two unsigned integers, returns 2^256 - 1 on overflow.
+     */
+    function mulCap(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring '_a' not being zero, but the
+        // benefit is lost if '_b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (_a == 0) return 0;
+
+        unchecked {
+            uint256 c = _a * _b;
+            return c / _a == _b ? c : UINT_MAX;
+        }
+    }
+
     receive() external payable {
-        ( , , address manager, , ) = market.marketInfo();
+        (, , address manager, , ) = market.marketInfo();
         if (msg.sender == manager) {
-            uint256 creatorReward = msg.value * creatorFee / DIVISOR;
-            requireSendXDAI(payable(creator), creatorReward);
+            creatorReward = (msg.value * creatorFee) / DIVISOR;
             poolReward = msg.value - creatorReward;
         }
     }
