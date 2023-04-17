5829691be7e60746eddbfc825698c80bdc309965
fnanni-0 <fnanni@itba.edu.ar>
2023-04-17 01:25:38 -0300

    chore: add distribution of liquidity rewards

--- scripts/distribute-rewards.js
+++ scripts/distribute-rewards.js
@@ -1,4 +1,5 @@
 const { gql, GraphQLClient } = require('graphql-request')
+const { BigNumber } = ethers;
 
 // ABI for transaction batcher contract.
 const BATCHER_ABI = [
@@ -16,6 +17,7 @@ const BATCHER_ABI = [
     type: 'function'
   }
 ]
+const ZERO = BigNumber.from(0);
 
 const transactionBatcher = "0xA73A872eFD768bb23efb24CEeB9e330bcCA259D6";
 const subgraphUrl = "https://api.thegraph.com/subgraphs/name/prodeapp/prodeapp";
@@ -41,6 +43,9 @@ async function main() {
           bets {
             claim
           }
+          manager {
+            id
+          }
         }
       }
     `, {
@@ -59,6 +64,9 @@ async function main() {
           submissionTimeout
           bets {
             claim
+          }
+          manager {
+            id
           }
         }
       }
@@ -79,11 +87,15 @@ async function main() {
 
   for (const market of markets) {
     console.log(`${market.name} (${market.id})`);
-    const datas = await processMarket(market.id, market.bets.filter(bet => bet.claim).length, signer);
+    const {datas, liquidityDatas} = await processMarket(market.id, market.bets.filter(bet => bet.claim).length, market.manager.id, signer);
 
     args.targets = args.targets.concat(Array(datas.length).fill(market.id));
     args.values = args.values.concat(Array(datas.length).fill(0));
     args.datas = args.datas.concat(datas);
+
+    args.targets = args.targets.concat(Array(liquidityDatas.length).fill(market.manager.id));
+    args.values = args.values.concat(Array(liquidityDatas.length).fill(0));
+    args.datas = args.datas.concat(liquidityDatas);
   }
 
   if (args.targets.length === 0) {
@@ -102,7 +114,15 @@ async function main() {
   console.log('distribution completed!');
 }
 
-async function processMarket(marketAddress, totalClaimed, signer) {
+async function processMarket(marketAddress, totalClaimed, managerAddress, signer) {
+  const LiquidityFactory = await ethers.getContractFactory("LiquidityFactory");
+  const liquidityFactory = await LiquidityFactory.attach("0x6bbe06d775445f052b9684d98f80161921e67d2a");
+  const exists = await liquidityFactory.exists(managerAddress);
+  const managerBalance = await ethers.provider.getBalance(managerAddress);
+  const marketBalance = await ethers.provider.getBalance(marketAddress);
+  if (marketBalance.eq(ZERO) && (!exists || managerBalance.eq(ZERO))) {
+    return {datas: [], liquidityDatas: []};  }
+
   let { bets, market: marketData } = await graph.request(
     gql`
       query rankingQuery($marketAddress: String) {
@@ -128,6 +148,7 @@ async function processMarket(marketAddress, totalClaimed, signer) {
   const market = await Market.attach(marketAddress);
 
   let datas = [];
+  let winnersData = [];
   let firstSharedIndex = 0;
   let endSharedIndex = 0;
   let rankIndex = 0;
@@ -144,13 +165,49 @@ async function processMarket(marketAddress, totalClaimed, signer) {
 
     for (let j=0; j<=endSharedIndex-firstSharedIndex; j++){
       // console.log('Claim inputs:', rankIndex, currentRankPoints, firstSharedIndex, endSharedIndex)
+      winnersData.push({
+        points: currentRankPoints,
+        rankIndex: rankIndex,
+        firstSharedIndex: firstSharedIndex,
+        endSharedIndex: endSharedIndex,
+      })
       const claimReward = (await market.populateTransaction.claimRewards(rankIndex++, firstSharedIndex, endSharedIndex)).data;
       datas.push(claimReward);
     }
 
-    if (endSharedIndex >= marketData.prizes.length) break;
+    if (endSharedIndex >= marketData.prizes.length - 1) break;
     firstSharedIndex = endSharedIndex+1
     endSharedIndex = firstSharedIndex
+  }
+
+  // Distribute liquidity fees
+  const liquidityDatas = [];
+  if (exists && managerBalance.gt(ZERO)) {
+    const LiquidityPool = await ethers.getContractFactory("LiquidityPool");
+    const liquidityPool = await LiquidityPool.attach(managerAddress);
+    const stakedEventFilter = liquidityPool.filters.Staked();
+    const stakedEvents = await liquidityPool.queryFilter(stakedEventFilter);
+    const stakers = [...new Set(stakedEvents.map(event => event.args._user))];
+
+    for (let j = 0; j < stakers.length; j++){
+      const claimLiquidityRewards = (await liquidityPool.populateTransaction.claimLiquidityRewards(stakers[j])).data;
+      liquidityDatas.push(claimLiquidityRewards);
+    }
+
+    const executeCreatorRewards = (await liquidityPool.populateTransaction.executeCreatorRewards()).data;
+    liquidityDatas.push(executeCreatorRewards);
+
+    const pointsToWin = Number(await liquidityPool.pointsToWin());
+    for (let j = 0; j < winnersData.length; j++){
+      if (winnersData[j].points >= pointsToWin) {
+        const claimBettorRewards = (await liquidityPool.populateTransaction.claimBettorRewards(
+          winnersData[j].rankIndex,
+          winnersData[j].firstSharedIndex,
+          winnersData[j].endSharedIndex
+        )).data;
+        liquidityDatas.push(claimBettorRewards);
+      }
+    }
   }
 
   if (datas.length === 0) {
@@ -167,12 +224,12 @@ async function processMarket(marketAddress, totalClaimed, signer) {
       }
     }
 
-    return datas;
+    return {datas: datas, liquidityDatas: liquidityDatas};
   }
 
   if (totalClaimed === datas.length) {
     console.log('prizes already claimed');
-    return [];
+    return {datas: [], liquidityDatas: liquidityDatas};
   }
 
   if (endSharedIndex < marketData.prizes.length) {
@@ -181,7 +238,7 @@ async function processMarket(marketAddress, totalClaimed, signer) {
     datas.push(remainingPrizes);
   }
 
-  return datas;
+  return {datas: datas, liquidityDatas: liquidityDatas};
 }
 
 main()
